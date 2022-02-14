9ae1fe0343ad31d9f477dbc42b532d571590c51c
Abdellah-05 <abdouelaaroub@gmail.com>
2022-02-14 14:11:14 +0100

    save get data

--- server_dataSet/app.py
+++ server_dataSet/app.py
@@ -1,16 +1,14 @@
+from crypt import methods
 from flask import Flask, jsonify
 from modules import getDataset
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
+@app.route("/dataset_kaggle", methods = ["GET"])
+def getDataset():
     a = str(input('name of dataset  :  '))
     newdata = getDataset.GetDataset()
-    alldata = newdata.dataset(a)
-    #print(alldata)
-    #newdata.download_Dataset(alldata[0]['url'], alldata[0]['ref'])
-    return jsonify(alldata)
+    return jsonify(newdata.dataset(a))
 
 if __name__ == '__main__':
     app.run(port=5000, host='0.0.0.0', debug=True)
